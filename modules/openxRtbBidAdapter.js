18078f52552d678b8df7050decef0618fd9dfa71
Jimmy Tu <jimmy.tu@openx.com>
2020-07-04 14:20:33 -0700

    Added unit tests Added vastUrl support for video requests Moved sync url to constant
    Response object missing:
    * transactionId
    * dspId
    * buyerId

--- modules/openxRtbBidAdapter.js
+++ modules/openxRtbBidAdapter.js
@@ -9,7 +9,8 @@ const bidderVersion = '1.0';
 const VIDEO_TARGETING = ['startdelay', 'mimes', 'minduration', 'maxduration',
   'startdelay', 'skippable', 'playbackmethod', 'api', 'protocols', 'boxingallowed',
   'linearity', 'delivery', 'protocol', 'placement', 'minbitrate', 'maxbitrate', 'ext'];
-const REQUEST_URL = 'https://rtb.openx.net/openrtbb/prebidjs';
+export const REQUEST_URL = 'https://rtb.openx.net/openrtbb/prebidjs';
+export const SYNC_URL = 'https://u.openx.net/w/1.0/pd?ph=2d1251ae-7f3a-47cf-bd2a-2f288854a0ba';
 
 export const spec = {
   code: 'openx',
@@ -213,20 +214,30 @@ function interpretResponse(resp, req) {
   }
 
   let bids = [];
-  respBody.seatbid.forEach(seatbid =>
-    bids = [...bids, ...seatbid.bid.map(bid => ({
-      requestId: bid.impid,
-      cpm: bid.price,
-      width: bid.w,
-      height: bid.h,
-      creativeId: bid.crid,
-      dealId: bid.dealid,
-      currency: respBody.cur || 'USD',
-      netRevenue: true,
-      ttl: 300,
-      ad: bid.adm,
-      mediaType: 'banner' in req.data.imp[0] ? BANNER : VIDEO
-    }))]);
+  respBody.seatbid.forEach(seatbid => {
+    bids = [...bids, ...seatbid.bid.map(bid => {
+      let response = {
+        requestId: bid.impid,
+        cpm: bid.price,
+        width: bid.w,
+        height: bid.h,
+        creativeId: bid.crid,
+        dealId: bid.dealid,
+        currency: respBody.cur || 'USD',
+        netRevenue: true,
+        ttl: 300,
+        mediaType: 'banner' in req.data.imp[0] ? BANNER : VIDEO
+      };
+
+      if (response.mediaType === VIDEO && bid.nurl) {
+        response.vastUrl = bid.nurl;
+      } else {
+        response.ad = bid.adm;
+      }
+
+      return response
+    })];
+  });
 
   return bids;
 }
@@ -241,7 +252,6 @@ function interpretResponse(resp, req) {
 function getUserSyncs(syncOptions, responses, gdprConsent, uspConsent) {
   if (syncOptions.iframeEnabled || syncOptions.pixelEnabled) {
     let pixelType = syncOptions.iframeEnabled ? 'iframe' : 'image';
-    let url = `https://u.openx.net/w/1.0/pd?ph=2d1251ae-7f3a-47cf-bd2a-2f288854a0ba`;
     let queryParamStrings = [];
     if (gdprConsent) {
       queryParamStrings.push('gdpr=' + (gdprConsent.gdprApplies ? 1 : 0));
@@ -252,7 +262,7 @@ function getUserSyncs(syncOptions, responses, gdprConsent, uspConsent) {
     }
     return [{
       type: pixelType,
-      url: `${url}${queryParamStrings.length > 0 ? '&' + queryParamStrings.join('&') : ''}`
+      url: `${SYNC_URL}${queryParamStrings.length > 0 ? '&' + queryParamStrings.join('&') : ''}`
     }];
   }
 }
